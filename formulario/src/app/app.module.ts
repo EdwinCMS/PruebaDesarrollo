11f5cb91f645f78eb1c993e6ebaf418dcaadae68
Edwin Moreno <edwinc_m1996@hotmail.com>
2020-08-23 12:24:20 -0500

    modulo autoria y obra creados

--- formulario/src/app/app.module.ts
+++ formulario/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AutoriaComponent } from './components/autoria/autoria.component';
+import { ObraComponent } from './components/obra/obra.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AutoriaComponent,
+    ObraComponent
   ],
   imports: [
     BrowserModule
